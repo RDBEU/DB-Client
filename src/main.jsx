c4001e0a9b7eb3047622e3f1181a6084b44b0904
Roberto Kyuchukov <rkyuchukov18@gmail.com>
2024-04-20 19:47:34 +0300

    fixing errors

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { AuthProviderWrapper } from "./context/auth.context";
 import './index.css'
 import {disableReactDevTools} from'@fvilers/disable-react-devtools'
 
-if(process.env.NODE_ENV === 'production') disableReactDevTools()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
